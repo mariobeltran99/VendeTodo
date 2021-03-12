fe13f4cefe00a745d2b9a29f297eb7c7d3708841
mariobeltran99 <mariobeltran1999@gmail.com>
2021-03-11 19:20:53 -0600

    Fixed Input

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,14 +24,14 @@
             <div class="w-full mx-auto bg-transparent" style="max-width: 300px">
                 <form>
                     <div class="w-full px-5 py-5 text-center">
-                        <div class="mb-6">
-                            <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-400"></label>
-                            <input type="text" name="name" id="name" placeholder="Ingrese su correo electrónico" required class="w-full px-3 py-2 placeholder-gray-500 border border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-gray-700 focus:border-gray-700 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+                        <div class="mb-6 w-full">
+                            <label for="name" class="block mb-4 text-gray-600"></label>
+                            <input type="text" name="name" id="name" placeholder="Correo Electrónico" required class="w-full px-3 py-2 placeholder-gray-500 border border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-gray-700 focus:border-gray-700 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                         </div>
                         <div class="mb-6 w-full">
                             <button type="button" class="focus:outline-none
                             text-white text-md py-2.5 px-5 p-1
-                            rounded-md bg-indigo-700 hover:bg-indigo-900 hover:shadow-lg">Vender o Comprar  <i class="fas fa-sign-in-alt"></i></button>
+                            rounded-md bg-indigo-700 hover:bg-indigo-900 hover:shadow-lg">Ingresar  <i class="fas fa-sign-in-alt"></i></button>
                         </div>
                     </div>
                 </form>
