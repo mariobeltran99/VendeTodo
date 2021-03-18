af6ad707af5b0637696048429d2bcea99274ccee
ErickAguilar009 <erickbvb009@gmail.com>
2021-03-17 18:46:34 -0600

    Configurando vista, controlador de ViewProduct

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,10 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\producto;
+use App\Models\usuario;
+use App\Models\telefono;
+use App\Models\imagen;
 
 class ProductoController extends Controller
 {
-    public function viewProduct(){
-        return view('viewProduct');
+    public function viewProduct($id){
+        $product = producto::where('id', $id)->get()->first();
+        $telefono=telefono::where('id', $product->id_telefono)->get()->first();
+        $product->user=usuario::where('id',$telefono->id_usuario)->get()->first();
+        $imagen=imagen::where('id_producto',$product->id)->get();
+        $product->telefono=$telefono;
+        $product->imagen=$imagen;
+     
+        return view('viewProduct', [
+            'product' => $product
+        ]);
     }
 }
+
