320a406fdca5aac8eced76530168b933bb6f4d8d
mariobeltran99 <mariobeltran1999@gmail.com>
2021-03-18 17:18:12 -0600

    Add new attributes in navigation

--- resources/views/homeLayout.blade.php
+++ resources/views/homeLayout.blade.php
@@ -3,6 +3,8 @@
     <div class="min-w-screen min-h-screen bg-gray-400 peeb-5 pt-20">
         <x-navigation
             flagR="{{ session('rol') }}"
+            named="{{ session('nombre') }}"
+            url="{{ session('foto') }}"
         />
         <main role="main">
             @yield('container')
