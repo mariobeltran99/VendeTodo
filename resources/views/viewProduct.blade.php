85d23e484080b78fd273eab7433083dcf3eac1e4
FherEnrique <enquique500@gmail.com>
2021-03-19 13:37:47 -0600

    Ultimos erorres

--- resources/views/viewProduct.blade.php
+++ resources/views/viewProduct.blade.php
@@ -1,5 +1,5 @@
-@extends('layout')
-@section('content')
+@extends('homeLayout')
+@section('container')
         <br>
         <h1>{{ $product->nombre }}</h1>
         <div>
@@ -8,15 +8,14 @@
             <h3>${{ $product->precio }}</h3>
             <h3>{{ $product->descripcion }}</h3>
             <br>
-            @if (empty($product->imagen))
-                <h3>NO HAY IMAGENES</h3>
+            @if (count($product->imagen) ==0)
+                <h1>No hay imagenes a mostrar</h1>
             @else
                 @foreach($product->imagen as $img)
                     <img src="https://www.usps.com/ecp/asset/images/O_BOX4-L0.jpg">
-                    <!-- <img src= asset('storage/' . $img->foto)}}> -->
+                  <!--   <img src= asset('storage/' . $img->foto)}}> -->
                     <br>
                 @endforeach
-
             @endif
         <br>
 @endsection
