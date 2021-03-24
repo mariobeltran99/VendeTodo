4034d1eaba877dc04bd036fab787e9c35be2bc63
ErickAguilar009 <erickbvb009@gmail.com>
2021-03-23 23:52:49 -0600

    Agregando funcionalidad de estrellas a valoraciones

--- resources/views/Product/viewProduct.blade.php
+++ resources/views/Product/viewProduct.blade.php
@@ -1,13 +1,38 @@
 @extends('..layouts.homeLayout')
+<style>
+.pro{
+    display:flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.pro > img{
+width: 20%;
+
+}
+</style>
+
 @section('container')
+
         <br>
         <h1>{{ $product->nombre }}</h1>
-        <div>
+        <div class = "pro">
             <h3>{{ $product->user->nombre }}</h3>
             <h3>{{ $product->user->nombre_usuario }}</h3>
             <h3>${{ $product->precio }}</h3>
             <h3>{{ $product->descripcion }}</h3>
             <h3>{{ $product->average }}</h3>
+           
+            @for ($i=0;$i < 5;$i++)
+                @if ($product->average < $i)
+                <img src="https://images.emojiterra.com/google/android-nougat/512px/2b50.png ">
+                @else
+                <img src="https://www.seekpng.com/png/detail/355-3555731_estrella-800x762-goomzilla-star.png ">
+
+                @endif
+
+            @endfor
+            </div>
             <br>
             @if (count($product->imagen) ==0)
                 <h1>No hay imagenes a mostrar</h1>
@@ -18,16 +43,18 @@
                     <br>
                 @endforeach
             @endif
+        
             <br>
             @if (count($product->valoracion) ==0)
                     <h1>No hay valoraciones a mostrar</h1>
-                @else
+            @else
                     @foreach($product->valoracion as $val)
                     <h3>{{ $val->id_usuario }}</h3>
                     <h3>{{ $val->estrella }}</h3>   
                     <h3>{{ $val->comentario }}</h3>
                         <br>
                     @endforeach
-                @endif
+            @endif
             <br>
+        
 @endsection
